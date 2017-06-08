1263853bab9bfd66cdc9c83bb80ad341f9940f0c
junechiu <s982535@gmail.com>
2017-06-08 21:33:24 +0800

    button and lightbox: css, js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-$(document).ready(function() {
+var handleIframeSize = function() {
     var height = $(window).height();
     var width = $(window).width();
     $('#iframe393citizen').css('min-height', height).css('width', width);
+};
+
+var toggleLightBoxActive = function() {
+    $('#chartLightBoxShadow').toggleClass('active');
+    $('#chartLightBox').toggleClass('active');
+};
+
+var initialLightBoxEvent = function() {
+    $('#chartLightBoxButton').on('click', toggleLightBoxActive);
+    $('#closeButton').on('click', toggleLightBoxActive);
+};
+
+$(document).ready(function() {
+
+    handleIframeSize();
+    initialLightBoxEvent();
+
 });
