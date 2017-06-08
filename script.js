2f151fd29519cfd6b431a7eef554892f913b789f
junechiu <s982535@gmail.com>
2017-06-08 20:02:51 +0800

    html : chart light box

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     var height = $(window).height();
     var width = $(window).width();
-    $('#iframe393citizen').css('height', height).css('width', width);
+    $('#iframe393citizen').css('min-height', height).css('width', width);
 });
