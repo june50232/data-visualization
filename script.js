608cd564d7879837af62a44c984c9ea4a1fb4d7f
junechiu <s982535@gmail.com>
2017-06-14 17:34:41 +0800

    youtube lightbox close function

--- script.js
+++ script.js
@@ -4,19 +4,39 @@ var handleIframeSize = function() {
     $('#iframe393citizen').css('min-height', height).css('width', width);
 };
 
-var toggleLightBoxActive = function() {
-    $('#chartLightBoxShadow').toggleClass('active');
-    $('#chartLightBox').toggleClass('active');
+var handleLightBoxSize = function() {
+    var width = $(window).width();
+    var height = (width / 2.2);
+    $('#lightBoxYoutubePlayer').css('width', width).css('min-height', height);
+};
+
+var renderIframe = function() {
+    $('#lightBoxYoutubePlayerContainer').append('<iframe id="lightBoxYoutubePlayer" src="https://www.youtube.com/embed/BTkM9CDQjc4" frameborder="0" allowfullscreen></iframe>');
+    handleLightBoxSize();
+};
+
+var removeIframe = function() {
+    $('#lightBoxYoutubePlayer').remove();
+};
+
+var renderLightBox = function() {
+    $('#lightBox').addClass('active');
+    renderIframe();
+};
+
+var closeLightBox = function() {
+    $('#lightBox').removeClass('active');
+    removeIframe();
 };
 
 var initialLightBoxEvent = function() {
-    $('#chartLightBoxButton').on('click', toggleLightBoxActive);
-    $('#closeButton').on('click', toggleLightBoxActive);
+    $('#lightBoxButton').on('click', renderLightBox);
+    $('#closeButton').on('click', closeLightBox);
 };
 
 $(document).ready(function() {
 
     handleIframeSize();
     initialLightBoxEvent();
-
+    handleLightBoxSize();
 });
